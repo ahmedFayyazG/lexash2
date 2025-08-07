479d59a41430cae463f80c6076cbdbfe6bd8fe63
Ahmed Fayyaz <ahmedwebmaster93@gmail.com>
2025-08-07 13:51:24 +0100

    immigration page

---
+++ src/app/personal-immigration-page/page.tsx
@@ -0,0 +1,25 @@
+
+
+import Breacrumb from '@/common/Breacrumb';
+import HeaderOne from '@/layouts/headers/HeaderOne';
+import React from 'react';
+import Cta2Area from '../../components/homes/home-3/Cta2Area';
+import FooterOne from '@/layouts/footers/FooterOne';
+import PersonalImmigrationPage from '@/components/personal-immigration/page';
+
+
+
+
+
+const SelfSponsorshipDetails = () => {
+  return (
+    <>
+      <HeaderOne />
+        <PersonalImmigrationPage/>
+      <Cta2Area />
+      <FooterOne />
+    </>
+  );
+};
+
+export default SelfSponsorshipDetails;
