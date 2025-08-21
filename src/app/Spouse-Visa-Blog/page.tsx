666b101f35581ee4431a2c7063e9b68bb5ff40dd
Ahmed Fayyaz <ahmedwebmaster93@gmail.com>
2025-08-21 11:01:18 +0100

    changes eleminate self spons

--- src/app/Spouse-Visa-Blog/page.tsx
+++ src/app/Spouse-Visa-Blog/page.tsx
@@ -5,7 +5,7 @@ import HeaderOne from '@/layouts/headers/HeaderOne';
 import React from 'react';
 import Cta2Area from '../../components/homes/home-3/Cta2Area';
 import FooterOne from '@/layouts/footers/FooterOne';
-import SpouseVisaBlogPostPage from './spouse-visa-blog';
+// import SpouseVisaBlogPostPage from './spouse-visa-blog';
 
 
 
@@ -16,7 +16,7 @@ const SelfSponsorshipDetails = () => {
   return (
     <>
       <HeaderOne />
-      <SpouseVisaBlogPostPage />
+      {/* <SpouseVisaBlogPostPage /> */}
       <Cta2Area />
       <FooterOne />
     </>
